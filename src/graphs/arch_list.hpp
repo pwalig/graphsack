587dac3880625fe0158833d89201d3d45dd9daf7
pwalig <pwalig03@gmail.com>
2025-03-31 12:54:21 +0200

    Try improve arch_list

--- src/graphs/arch_list.hpp
+++ src/graphs/arch_list.hpp
@@ -1,27 +1,47 @@
 #pragma once
 #include <utility>
-#include <vector>
+#include <cassert>
 
 namespace gs {
 	namespace graphs {
 		template <typename T = size_t>
 		class arch {
 		public:
-			using index_type = T;
-			index_type from;
-			index_type to;
+			using value_type = T;
+			value_type from;
+			value_type to;
 		};
 
 		template <typename T = size_t>
+		using vertex_pair = std::pair<T, T>;
+
+		template <class Container>
 		class arch_list {
 		public:
-			using index_type = T;
-			using arch_type = std::pair<index_type, index_type>;
+			using value_type = typename Container::value_type;
+			using reference = typename Container::reference;
+			using const_reference = typename Container::const_reference;
+			using size_type = typename Container::size_type;
+			using iterator = typename Container::iterator;
+			using const_iterator = typename Container::iterator;
 
-			std::vector<arch_type> list;
+		private:
+			Container storage;
+			size_type n;
 
-			const size_t size() const { return list.size(); }
-			const size_t capacity() const { return list.capacity(); }
+		public:
+			inline arch_list(size_type N, std::initializer_list<value_type> init) : storage(init), n(N) { }
+
+			const size_type vertices() const { return n; }
+			const size_type arches() const { return storage.size() / 2; }
+			const size_type size() const { return storage.size(); }
+			const size_type capacity() const { return storage.capacity(); }
+
+			inline reference operator[] (size_type i) { return storage[i]; }
+			inline const_reference operator[] (size_type i) const { return storage[i]; }
+
+			inline iterator begin() { return storage.begin(); }
+			inline iterator end() { return storage.end(); }
 		};
 	}
 }
