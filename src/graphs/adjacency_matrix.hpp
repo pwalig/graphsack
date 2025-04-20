1b65d1686be45799b21565aacd353c36ec061f0e
pwalig <pwalig03@gmail.com>
2025-04-20 18:08:06 +0200

    Move part of adjacency_matrix definition to separate source file

--- src/graphs/adjacency_matrix.hpp
+++ src/graphs/adjacency_matrix.hpp
@@ -22,17 +22,7 @@ namespace gs {
 			std::vector<bool> storage;
 		public:
 			adjacency_matrix() = default;
-			inline adjacency_matrix(std::initializer_list<std::initializer_list<bool>> init)
-				: storage(init.size()* init.begin()->size()) {
-				size_type i = 0;
-				for (const auto& w : init) {
-					assert(w.size() == init.size());
-					for (bool c : w) {
-						storage[i] = c;
-						++i;
-					}
-				}
-			}
+			adjacency_matrix(std::initializer_list<std::initializer_list<bool>> init);
 
 			inline adjacency_matrix(size_type n) : storage(n * n) { }
 
@@ -72,89 +62,9 @@ namespace gs {
 				return res;
 			}
 
-			inline static adjacency_matrix from_graph6(const std::string& buff) {
-				char bit = 32;
-				typename std::string::size_type poz = 0;
-				size_type n = 0;
-				if (buff[0] == '~') {
-					if (buff[1] == '~') {
-						poz = 2;
-						while (poz < 8) {
-							n <<= 6;
-							n += buff[poz++] - 63;
-						}
-					} else {
-						poz = 1;
-						while (poz < 4) {
-							n <<= 6;
-							n += buff[poz++] - 63;
-						}
-					}
-				}
-				else {
-					n = buff[poz++] - 63;
-				}
-				adjacency_matrix res(n);
-				for (size_type i = 1; i < n; i++) {
-					for (size_type j = 0; j < i; j++)
-					{
-						if (bit == 0) { bit = 32;  poz++; }
-						if ((buff[poz] - 63) & bit)
-							res.at(i, j) = res.at(j, i) = true;
-						else
-							res.at(i, j) = res.at(j, i) = false;
-						bit >>= 1;
-					}
-				}
-				return res;
-			}
+			static adjacency_matrix from_graph6(const std::string& buff);
 
-			inline std::string graph6() {
-				size_type n = size();
-				std::string result;
-
-				if (n <= 62) {
-					result += (char)(n + 63);
-				}
-				else if (n <= 258047) {
-					result += '~';
-					for (size_type i = 1 << 12; i > 0; i >>= 6) {
-						result += (char)(((n / i) % 64) + 63);
-					}
-				}
-				else if (n <= 68719476735) {
-					result += "~~";
-					for (size_type i = 1 << 12; i > 0; i >>= 6) {
-						result += (char)(((n / i) % 64) + 63);
-					}
-				}
-				else throw std::logic_error("graph size is to large for graph6 representation");
-
-				std::vector<bool> bitVector;
-				for (size_type j = 1; j < n; ++j) {
-					for (size_type i = 0; i < j; ++i) {
-						bitVector.push_back(at(i, j));
-					}
-				}
-
-				// Make the length a multiple of 6 by padding with 0s
-				size_type len = bitVector.size();
-				while (len % 6 != 0) {
-					bitVector.push_back(false);
-					len++;
-				}
-
-				// Convert every 6 bits to a base64 character
-				for (size_type i = 0; i < len; i += 6) {
-					unsigned char val = 0;
-					for (size_type j = 0; j < 6; ++j) {
-						val = val * 2 + (bitVector[i + j] ? 1 : 0);
-					}
-					result += (char)(val + 63);
-				}
-
-				return result;
-			}
+			std::string graph6();
 
 			template <typename T>
 			inline std::vector<T> flatten() const {
